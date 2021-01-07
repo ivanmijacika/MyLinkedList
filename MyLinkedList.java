86c11ca0abfd7defa48efb4db21aa630cabce250
ivanmijacika <imijacika20@stuy.edu>
2021-01-06 21:29:01 -0500

    add added

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,10 +4,34 @@ public class MyLinkedList{
   public MyLinkedList(){
     size = 0;
   }
-  public int size();
-  public boolean add(String value);
-  public void add(int index, String value);
-  public String get(int index);
-  public String set(int index, String value);
-  public String toString();
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String value){
+    if (size==0){
+      Node n = new Node(value);
+      start = n;
+      end = n;
+      size++;
+    }
+    else {
+      Node n = new Node(value);
+      n.setPrev(end);
+      end.setNext(n);
+      end = n;
+      size++;
+    }
+    return true;
+  }
+
+  public void add(int index, String value){
+  }
+  public String get(int index){
+  }
+  public String set(int index, String value){
+  }
+  public String toString(){
+  }
 }
