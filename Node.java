31e1d293900d03204f00d95d082867b97c92236e
ivanmijacika <imijacika20@stuy.edu>
2021-01-06 20:14:11 -0500

    get and set added for all instance variables

--- Node.java
+++ Node.java
@@ -5,4 +5,27 @@ public class Node{
   private String data;
   private Node next,prev;
 
+  public String getData(){
+    return data;
+  }
+
+  public void setData(String data){
+    this.data = data;
+  }
+
+  public Node getNext(){
+    return next;
+  }
+
+  public void setNext(Node next){
+    this.next = next;
+  }
+
+  public Node getPrev(){
+    return prev;
+  }
+
+  public void setPrev(Node Prev){
+    this.prev = prev;
+  }
 }
