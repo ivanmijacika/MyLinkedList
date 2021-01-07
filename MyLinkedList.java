56f0fdd4a8877d85f55081eb40907a4e6916e5ee
ivanmijacika <imijacika20@stuy.edu>
2021-01-06 20:25:07 -0500

    constructor added to MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,13 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
-
-
- public int size();
- public boolean add(String value);
- public void add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+  private int size;
+  private Node start,end;
+  public MyLinkedList(){
+    size = 0;
+  }
+  public int size();
+  public boolean add(String value);
+  public void add(int index, String value);
+  public String get(int index);
+  public String set(int index, String value);
+  public String toString();
 }
